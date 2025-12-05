43634f1587c384d5138597be2eb6c62d47a92588
Ícaro Barros <icarojsbarros@gmail.com>
2025-12-05 03:14:13 -0300

    Fix: Update instance type to t3.micro for Free Tier eligibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,14 +22,14 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open to the world (Web)
+    cidr_blocks = ["0.0.0.0/0"]   # Open to the world (Web)
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH Access
+    cidr_blocks = ["0.0.0.0/0"]   # SSH Access
   }
 
   egress {
@@ -43,7 +43,7 @@ resource "aws_security_group" "web_sg" {
 # 3. Create EC2 Instance
 resource "aws_instance" "web_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"   # Free Tier eligible
+  instance_type = "t3.micro"   # Free Tier eligible
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
